3dc3ebec30ebcff2d74b5878c2013dbdb6d3a419
Ondřej Váně <onry.vane@gmail.com>
2020-04-13 18:23:53 +0200

    Struktura FE
    - přidány stránky home a about
    - přidán navigační panel
    - rozdělení projektu do adresářů
    -vytvořen query service

--- FE/KIV-IR/src/app/app.module.ts
+++ FE/KIV-IR/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './components/nav/main-nav/main-nav.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
