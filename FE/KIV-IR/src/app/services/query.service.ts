d2b9568f7a3296de60ec19e807cdf47048f73baa
Ondřej Váně <onry.vane@gmail.com>
2020-04-13 18:30:47 +0200

    Implementace service pro odesílání dotazu na BE

--- FE/KIV-IR/src/app/services/query.service.ts
+++ FE/KIV-IR/src/app/services/query.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {SearchRequest} from '../model/QueryRequest';
+import {SearchResponse} from '../model/QueryResponse';
+import {Observable} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders( {
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class QueryService {
 
-  constructor() { }
+  backendUrl = 'http://localhost:8080';
+  searchUrl = '/';
+
+  constructor(private httpClient: HttpClient) {}
+
+  searchPost(searchRequest: SearchRequest): Observable<SearchResponse> {
+    return this.httpClient.post<SearchResponse>(this.backendUrl + this.searchUrl, searchRequest, httpOptions);
+  }
 }
